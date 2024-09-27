e001ec0bc18dde537a36da387d82f8babc165087
ArnabTo <arnabs929@gmail.com>
2024-09-27 21:02:00 +0600

    added cta section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AnimatedCursor from 'react-animated-cursor'
 import './App.css'
 import About from './components/About'
 import Benefits from './components/Benefits'
@@ -6,6 +7,7 @@ import Navbar from './components/Navbar'
 import Pricing from './components/Pricing'
 import Services from './components/Services'
 import Testimonies from './components/Testimonies'
+import StartJourney from './components/StartJourney'
 
 function App() {
 
@@ -18,6 +20,28 @@ function App() {
       <Benefits/>
       <Testimonies/>
       <Pricing/>
+      <StartJourney/>
+       <AnimatedCursor
+        innerSize={8}
+        outerSize={10}
+        color='0, 0, 0'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+        />
     </div>
   )
 }
---
+++ src/components/StartJourney.jsx
@@ -0,0 +1,24 @@
+import CtaSvg from '../assets/undraw_meditating_re_aiqa.svg'
+import { Button } from './ui/button';
+const StartJourney = () => {
+    return (
+        <div className="max-w-7xl mx-auto mt-16 mb-24 px-6 md:px-12 lg:mt-40 lg:mb-44">
+        <div className="flex flex-col gap-10 lg:flex-row-reverse items-center">
+            <div className="flex flex-col justify-center items-start space-y-3 lg:px-16">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center lg:text-left">
+                Ready to Begin Your Meditation Journey?
+                </h1>
+                <p className="font-lora text-center lg:text-left">
+                Join hundreds of others in finding your inner peace.
+                </p>
+             <Button className='font-lora'>Book a Session</Button>
+            </div>
+            <div className="flex justify-center mb-6 lg:mb-0">
+                <img className="w-72 md:w-96 lg:w-[30rem] lg:max-w-2xl animate-float" src={CtaSvg} alt="hero" />
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default StartJourney;
